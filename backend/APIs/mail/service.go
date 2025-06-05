8a0612c7498ba0917f2929612cbf279423f85a79
jms-guy <guyjamie92@gmail.com>
2025-06-05 13:37:25 -0400

    Working on email service

---
+++ backend/APIs/mail/service.go
@@ -0,0 +1,83 @@
+package mail
+
+import (
+	"os"
+	"github.com/d-vignesh/go-jwt-auth/utils"
+	"github.com/hashicorp/go-hclog"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+/*
+	This package controls the email service for sending email verification codes necessary for account verification,
+	as well as password resetting
+*/
+
+//Interface for the mail service
+type MailService interface {
+	SendMail(mailReq *Mail) error
+	NewMail(from string, to string, subject, body string, data *MailData) *Mail
+}
+
+//Structure of the data to be used in the template of the mail
+type MailData struct {
+	Username	string
+	Code		string
+}
+
+//Email request struct
+type Mail struct {
+	From 		string
+	To 			string
+	Subject 	string
+	Body 		string
+	Data 		*MailData
+}
+
+//Sendgrid implementation of mailservice
+type SGMailService struct {
+	Logger 		hclog.Logger
+	Client		*sendgrid.Client
+}
+
+//Returns a new instance of SGMailService
+func NewSGMailService(logger hclog.Logger, configs *utils.Configurations) *SGMailService {
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	return &SGMailService{
+		Logger: logger,
+		Client: client,
+	}
+}
+
+//CreateMail takes in mail request, and constructs a sendgrid mail type
+func (ms *SGMailService) SendMail(mailReq *Mail) error {
+
+	from := mail.NewEmail("Greed", mailReq.From)
+	subject := mailReq.Subject
+	to  := mail.NewEmail(mailReq.Data.Username, mailReq.To)
+	plainTextContent := mailReq.Body
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, "")
+
+	response, err := ms.Client.Send(message)
+	if err != nil {
+		ms.Logger.Error("Failed to send email", "error", err)
+		return err
+	} else {
+		ms.Logger.Info("Email sent successfully", 
+        "statusCode", response.StatusCode,
+        "to", mailReq.To)
+	}
+
+	return nil
+}
+
+//NewMail returns a new mail request
+func (ms *SGMailService) NewMail(from string, to string, subject, body string, data *MailData) *Mail {
+	return &Mail{
+		From:		from,
+		To: 		to,
+		Subject:	subject,
+		Body:  		body,
+		Data:		data,
+	}
+}
