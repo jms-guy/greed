4027014aa6350a46bbcdffa9606dffd23327031a
jms-guy <guyjamie92@gmail.com>
2025-06-07 14:28:21 -0400

    Fixed gitignore issue

---
+++ backend/internal/utils/enums.go
@@ -0,0 +1,25 @@
+package utils
+/*
+import (
+
+)
+
+//A currency enum struct, containing the valid currencies supported in this application
+type Currency int
+
+const (
+	CAD Currency = iota
+	USD
+	EUR
+	GBP
+)
+
+type TransactionType int
+
+const (
+	credit TransactionType = iota
+	debit
+	transfer
+)
+
+*/
---
+++ backend/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"os"
+	"github.com/jms-guy/greed/backend/server"
+)
+
+func main() {
+	if err := server.Run(); err != nil {
+		os.Exit(1)
+	}
+}
